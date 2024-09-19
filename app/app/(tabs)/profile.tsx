b192389b1150e7bab20311e87f36becf6fcef506
Juan Lavagnini <91229986+juanlavagnini@users.noreply.github.com>
2024-09-18 21:03:40 -0300

    basic profile design

--- app/app/(tabs)/profile.tsx
+++ app/app/(tabs)/profile.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.container}>
+      <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={styles.image}/>
+      <Text style={styles.text}>Trini</Text>
     </View>
   )
 }
 
 export default profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    alignItems: "center",
+    gap: 20,  
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 100,
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
