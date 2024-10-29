b26f41485b8f582be012aa0ae6bb2f2dffe874fa
Juan Lavagnini <91229986+juanlavagnini@users.noreply.github.com>
2024-10-28 23:22:05 -0300

    Update user.tsx

--- app/app/(tabs)/profile/user.tsx
+++ app/app/(tabs)/profile/user.tsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-nativ
 import React, { useState } from 'react'
 import { Colors } from '@/constants/Colors'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useUserContext } from '@/app/_layout';
 
 
 const user = () => {
   const [id, setId] = useState<string | null>(null);
+  const { user } = useUserContext();
 
   React.useEffect(() => {
     const fetchId = async () => {
@@ -23,7 +25,7 @@ const user = () => {
   return (
       <View style={styles.container}>
           <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={styles.image}/>
-          <Text>{id}</Text>
+          <Text>{user?.email}</Text>
       </View>
   )
 }
